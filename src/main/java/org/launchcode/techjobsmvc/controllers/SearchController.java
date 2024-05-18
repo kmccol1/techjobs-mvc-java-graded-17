e2d2205b09a242bcc43ae43c490b40219b136769
Kyle McColgan <kdmbus21@gmail.com>
2024-05-17 21:13:46 -0500

    BONUS #1: Fix user's searchType not persisting post form submission.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -4,10 +4,7 @@ import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -44,6 +41,7 @@ public class SearchController {
 
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
 
         return "search";
     }
