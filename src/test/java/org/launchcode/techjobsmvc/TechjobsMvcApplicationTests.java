82aef1f8456dfda5d65061a5d189d71797298bb7
Kyle McColgan <kdmbus21@gmail.com>
2024-05-22 17:13:30 -0500

    Test tests: .

--- src/test/java/org/launchcode/techjobsmvc/TechjobsMvcApplicationTests.java
+++ src/test/java/org/launchcode/techjobsmvc/TechjobsMvcApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TechjobsMvcApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class TechjobsMvcApplicationTests
+{
+//	@Test
+//	public void runTests()
+//	{
+//		TestTaskTwo testFile1 = new TestTaskTwo();
+//		testFile1.testJobListingLoads();
+//
+//	}
 }
